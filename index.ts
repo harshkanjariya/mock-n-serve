3e295539f9d12b9034c31ac2ff4beb13fa5ceeb5
Harsh Kanjariya <code.harshkanjairya@gmail.com>
2022-06-15 10:18:45 +0530

    set-cookie and response type added

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
+import {CookieOptions} from "express";
+import * as e from 'express';
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
 
-import * as e from 'express';
 
 app.use(require('cors')({
     origin: true,
@@ -16,8 +18,38 @@ app.use(require('express-fileupload')({
 app.use(require('cookie-parser')());
 app.set('view engine', 'ejs');
 
+type CookieData = {
+    name: string;
+    value: string;
+    options: CookieOptions;
+}
+
+function setCookie(data: CookieData, res: e.Response) {
+    res.cookie(data.name, data.value, data.options);
+    res.send('success');
+}
+
+app.get('/set-cookie', (req: e.Request, res: e.Response) => {
+    const {name, value, ...options}: any = req.query;
+    setCookie({
+        name,
+        value,
+        options,
+    }, res);
+});
 app.use('*', (req: e.Request, res: e.Response) => {
-    res.render('output.ejs', req);
+    if (req.headers['response-type'] === 'application/json') {
+        res.send({
+            method: req.method,
+            url: req.originalUrl,
+            body: req.body,
+            query: req.query,
+            params: req.params,
+            cookies: req.cookies,
+        });
+    } else {
+        res.render('output.ejs', req);
+    }
 });
 
 app.listen(process.env.PORT, () => {
