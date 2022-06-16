685ecec760f0e85d63160a9e7a19f72dae074ca5
Harsh Kanjariya <code.harshkanjairya@gmail.com>
2022-06-16 15:21:40 +0530

    req.files added to output

--- index.ts
+++ index.ts
@@ -29,6 +29,21 @@ function setCookie(data: CookieData, res: e.Response) {
     res.send('success');
 }
 
+declare global {
+    export namespace Express {
+        interface Request {
+            files: any;
+            fileInfo?: {
+                [key: string]: {
+                    name: string;
+                    mimetype: string;
+                    size: number;
+                }
+            }
+        }
+    }
+}
+
 app.get('/set-cookie', (req: e.Request, res: e.Response) => {
     const {name, value, ...options}: any = req.query;
     setCookie({
@@ -38,6 +53,19 @@ app.get('/set-cookie', (req: e.Request, res: e.Response) => {
     }, res);
 });
 app.use('*', (req: e.Request, res: e.Response) => {
+    if ('files' in req) {
+        req.fileInfo = {};
+        Object.keys(req['files']).forEach((fileKey) => {
+            const file = req['files'][fileKey];
+            if (req.fileInfo) {
+                req.fileInfo[fileKey] = {
+                    name: file.name,
+                    mimetype: file.mimetype,
+                    size: file.size,
+                }
+            }
+        });
+    }
     if (req.headers['response-type'] === 'application/json') {
         res.send({
             method: req.method,
@@ -46,6 +74,7 @@ app.use('*', (req: e.Request, res: e.Response) => {
             query: req.query,
             params: req.params,
             cookies: req.cookies,
+            files: req['fileInfo'],
         });
     } else {
         res.render('output.ejs', req);
